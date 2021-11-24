ba9c0959b66b6225c8eb6d8aeb2c0b5e15208285
Leopold Yang <yangchengniubi@gmail.com>
2021-11-24 15:49:39 +0800

    main

--- MidTermExam/debug.py
+++ MidTermExam/debug.py
@@ -1,16 +1,11 @@
-import math
-x = float(input())
+fn = lambda *p:print(p[1]+p[2]+'|',end='')
 
-def sqrtN(x,n):
-    for _ in range(n):
-        x = math.sqrt(x)
-    return x
+def fun(n,x,y,z):
+    if n == 1:
+        fn(1,x,y)
+    else:
+        fun(n-1,x,z,y)
+        fn(1,x,y)
+        fun(n-1,z,y,x)
 
-if x > 10:
-    y = math.sin(x) + math.sqrt(x**2+1)
-elif 0 < x and x <= 10:
-    y = math.e ** x + sqrtN(x,5)
-elif x <= 0:
-    y = math.cos(x) - x**3 + 6*x
-
-print('%.2f'%y)
+fun(3,'A','B','T')
---
+++ test4/test.py
@@ -0,0 +1,12 @@
+with open("Answer3.txt",'w') as aw3:
+    count = len(open('test.py', 'r').readlines())
+    file = open('test.py', 'r')
+    strList = file.readlines()
+    maxEle = 0
+    for i in strList:
+        if len(i) > maxEle:
+            maxEle = len(i)
+    num = 1
+    for i in range(len(strList)):
+        ele = strList[i][:-1] + ' '*(maxEle-len(strList[i])) + '#' +str(i)
+        aw3.write(ele+'\n')
