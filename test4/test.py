0967e96945d99d71b3ef96b835e0f7b98fffe6cc
LeopoldY <58126004+LeopoldY@users.noreply.github.com>
2021-11-25 10:49:21 +0800

    Update test.py

--- test4/test.py
+++ test4/test.py
@@ -7,6 +7,6 @@ with open("Answer3.txt",'w') as aw3:
         if len(i) > maxEle:
             maxEle = len(i)
     num = 1
-    for i in range(len(strList)):
+    for i in range(count):
         ele = strList[i][:-1] + ' '*(maxEle-len(strList[i])) + '#' +str(i)
         aw3.write(ele+'\n')
